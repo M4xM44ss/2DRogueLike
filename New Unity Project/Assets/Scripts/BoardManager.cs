e6814d6539cc802f4041586c5770d0a5adaebe5b
Max Maass <maximilianomaassagnesi@gmail.com>
2018-02-06 14:44:20 -0600

    Fin Del Video 4 BoardManager y creé el Script GameManager

--- New Unity Project/Assets/Scripts/BoardManager.cs
+++ New Unity Project/Assets/Scripts/BoardManager.cs
@@ -1,20 +1,18 @@
 ﻿using UnityEngine;
 using System;
-using System.Collections.Generic;       //Allows us to use Lists.
-using Random = UnityEngine.Random;      //Tells Random to use the Unity Engine random number generator.
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
 
 
-{
 
 	public class BoardManager : MonoBehaviour
 	{
+	[Serializable]
 		public class Count
 		{
-			public int minimum;             //Minimum value for our Count class.
-			public int maximum;             //Maximum value for our Count class.
+			public int minimum;
+			public int maximum;
 
-
-			//Assignment constructor.
 			public Count (int min, int max)
 			{
 				minimum = min;
@@ -23,56 +21,48 @@ using Random = UnityEngine.Random;      //Tells Random to use the Unity Engine r
 		}
 
 
-		public int columns = 8;                                         //Number of columns in our game board.
-		public int rows = 8;                                            //Number of rows in our game board.
-		public Count wallCount = new Count (5, 9);                      //Lower and upper limit for our random number of walls per level.
-		public Count foodCount = new Count (1, 5);                      //Lower and upper limit for our random number of food items per level.
-		public GameObject exit;                                         //Prefab to spawn for exit.
-		public GameObject[] floorTiles;                                 //Array of floor prefabs.
-		public GameObject[] wallTiles;                                  //Array of wall prefabs.
-		public GameObject[] foodTiles;                                  //Array of food prefabs.
-		public GameObject[] enemyTiles;                                 //Array of enemy prefabs.
-		public GameObject[] outerWallTiles;                             //Array of outer tile prefabs.
+		public int columns = 8;
+		public int rows = 8;
+		public Count wallCount = new Count (5, 9);
+		public Count foodCount = new Count (1, 5); 
+		public GameObject exit;
+		public GameObject[] floorTiles;
+		public GameObject[] wallTiles;
+		public GameObject[] foodTiles;
+		public GameObject[] enemyTiles;
+		public GameObject[] outerWallTiles;
 
-		private Transform boardHolder;                                  //A variable to store a reference to the transform of our Board object.
-		private List <Vector3> gridPositions = new List <Vector3> ();   //A list of possible locations to place tiles.
+		private Transform boardHolder;                                  
+		private List <Vector3> gridPositions = new List <Vector3> ();   
 
 
-		//Clears our list gridPositions and prepares it to generate a new board.
 		void InitialiseList ()
 		{
-			//Clear our list gridPositions.
+		
 			gridPositions.Clear ();
 
-			//Loop through x axis (columns).
 			for(int x = 1; x < columns-1; x++)
 			{
-				//Within each column, loop through y axis (rows).
 				for(int y = 1; y < rows-1; y++)
 				{
-					//At each index add a new Vector3 to our list with the x and y coordinates of that position.
+					
 					gridPositions.Add (new Vector3(x, y, 0f));
 				}
 			}
 		}
-
-
-		//Sets up the outer walls and floor (background) of the game board.
+		
 		void BoardSetup ()
 		{
-			//Instantiate Board and set boardHolder to its transform.
 			boardHolder = new GameObject ("Board").transform;
 
-			//Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
+			
 			for(int x = -1; x < columns + 1; x++)
 			{
-				//Loop along y axis, starting from -1 to place floor or outerwall tiles.
+			
 				for(int y = -1; y < rows + 1; y++)
 				{
-					//Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
 					GameObject toInstantiate = floorTiles[Random.Range (0,floorTiles.Length)];
 
-					//Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
 					if(x == -1 || x == columns || y == -1 || y == rows)
 						toInstantiate = outerWallTiles [Random.Range (0, outerWallTiles.Length)];
 
@@ -134,20 +124,18 @@ using Random = UnityEngine.Random;      //Tells Random to use the Unity Engine r
 			//Reset our list of gridpositions.
 			InitialiseList ();
 
-			//Instantiate a random number of wall tiles based on minimum and maximum, at randomized positions.
+			
 			LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
 
-			//Instantiate a random number of food tiles based on minimum and maximum, at randomized positions.
+			
 			LayoutObjectAtRandom (foodTiles, foodCount.minimum, foodCount.maximum);
 
-			//Determine number of enemies based on current level number, based on a logarithmic progression
+			
 			int enemyCount = (int)Mathf.Log(level, 2f);
 
-			//Instantiate a random number of enemies based on minimum and maximum, at randomized positions.
-			LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
 
-			//Instantiate the exit tile in the upper right hand corner of our game board
+			LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
+	
 			Instantiate (exit, new Vector3 (columns - 1, rows - 1, 0f), Quaternion.identity);
 		}
 	}
-}
