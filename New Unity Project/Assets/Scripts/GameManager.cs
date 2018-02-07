cd5c5a1d64ca70d8ea72f104a4ea993027c7be98
Max Maass <maximilianomaassagnesi@gmail.com>
2018-02-07 13:56:47 -0600

    Video 5 a la mitad

--- New Unity Project/Assets/Scripts/GameManager.cs
+++ New Unity Project/Assets/Scripts/GameManager.cs
@@ -1,16 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+public class GameManager : MonoBehaviour
+{
 
-public class GameManager : MonoBehaviour {
+	public static GameManager instance = null;   
 
-	// Use this for initialization
-	void Start () {
+	private BoardManager boardScript;
+
+	private int level = 3;
+
+	void Awake()
+	{
+		if (instance == null)
+
+			instance = this;
 		
+		else if (instance != this)
+
+		Destroy(gameObject);    
+		
+		DontDestroyOnLoad(gameObject);
+
+		boardScript = GetComponent<BoardManager>();
+
+		InitGame();
 	}
+
+
+	void InitGame()
+	{
 	
-	// Update is called once per frame
-	void Update () {
+		boardScript.SetupScene(level);
+
+	}
 		
+	void Update()
+	{
 	}
 }
