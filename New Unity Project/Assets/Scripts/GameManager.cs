e51c11a188cd48ed7320a3332d95d40200be2812
Max Maass <maximilianomaassagnesi@gmail.com>
2018-02-25 17:16:57 -0600

    Modifiqué el C# de GameManager y Creé el C# de Player

--- New Unity Project/Assets/Scripts/GameManager.cs
+++ New Unity Project/Assets/Scripts/GameManager.cs
@@ -4,34 +4,29 @@ public class GameManager : MonoBehaviour
 {
 
 	public static GameManager instance = null;   
-
 	private BoardManager boardScript;
-
 	private int level = 3;
+	public int playerFoodPoints = 100;
+	[HideInInspector] public bool playersTurn = true;
 
 	void Awake()
 	{
 		if (instance == null)
-
 			instance = this;
-		
 		else if (instance != this)
-
 		Destroy(gameObject);    
-		
 		DontDestroyOnLoad(gameObject);
-
 		boardScript = GetComponent<BoardManager>();
-
 		InitGame();
 	}
-
-
 	void InitGame()
 	{
-	
 		boardScript.SetupScene(level);
+	}
 
+	public void GameOver()
+	{
+		enabled = false;
 	}
 		
 	void Update()
