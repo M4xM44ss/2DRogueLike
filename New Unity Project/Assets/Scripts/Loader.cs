e14bacc697d5a80e58bdf408a65d4ce9ec52347a
Max Maass <maximilianomaassagnesi@gmail.com>
2018-03-06 12:16:04 -0600

    Terminé el video 10 enfocadoen el C# Enemy ya corregido

--- New Unity Project/Assets/Scripts/Loader.cs
+++ New Unity Project/Assets/Scripts/Loader.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Loader : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+public class Loader : MonoBehaviour 
+{
+	public GameObject gameManager;
+
+
+	void Awake ()
+	{
+		if (GameManager.instance == null)
+			Instantiate(gameManager);
 	}
 }
